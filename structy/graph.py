0c1c0a5d6561f989b906bb4ac5c8dae9bc39cb66
laurencary <armstronglg720@gmail.com>
2023-07-28 18:49:54 -0700

    more graph problems

--- structy/graph.py
+++ structy/graph.py
@@ -19,4 +19,107 @@ graph = {
     'k': []
 }
 
-print(has_path(graph, 'f', 'k')) # True
+# print(has_path(graph, 'f', 'k')) # True
+
+
+
+def connected_components_count(graph):
+    visited = set()
+    count = 0
+    for node, edges in graph.items():
+        if node not in visited:
+            count += 1
+
+            stack = [node]
+            while stack:
+                print(stack)
+                curr = stack.pop()
+                visited.add(curr)
+                for cn in graph[curr]:
+                    if cn not in visited:
+                        stack.append(cn)
+            
+    return count
+
+
+# print(connected_components_count({
+#     0: [8, 1, 5],
+#     1: [0],
+#     5: [0, 8],
+#     8: [0, 5],
+#     2: [3, 4],
+#     3: [2, 4],
+#     4: [3, 2]
+# })) # -> 2
+
+
+
+def largest_component(graph):
+	max_node = 0
+	visited = set()
+	
+	for node, edges in graph.items():
+		if node not in visited:
+			node_count = 0
+			visited.add(node)      
+			stack = [node]
+			while stack:
+				curr = stack.pop()
+				print(graph[curr])
+				node_count += 1
+				for neighbor in graph[curr]:
+					if neighbor not in visited:
+						visited.add(neighbor)
+						stack.append(neighbor)
+		max_node = max(max_node, node_count)
+	
+	return max_node
+
+
+# print(largest_component({
+#   3: [],
+#   4: [6],
+#   6: [4, 5, 7, 8],
+#   8: [6],
+#   7: [6],
+#   5: [6],
+#   1: [2],
+#   2: [1]
+# })) # -> 6
+
+
+from collections import defaultdict
+from collections import deque
+
+def shortest_path(edges, node_A, node_B):
+  adj_dict = defaultdict(list)
+  
+  for neighbors in edges:
+    adj_dict[neighbors[0]].append(neighbors[1])
+    adj_dict[neighbors[1]].append(neighbors[0])
+    
+  visited = set(node_A)
+  queue = deque([(node_A, 0)])
+
+  while queue:
+    curr, level = queue.popleft()
+    if curr == node_B:
+      return level
+    
+    for neighbor in adj_dict[curr]:
+      if neighbor not in visited:
+        queue.append((neighbor, level + 1))
+        visited.add(neighbor)
+        
+  return -1
+  
+    
+edges = [
+  ['w', 'x'],
+  ['x', 'y'],
+  ['z', 'y'],
+  ['z', 'v'],
+  ['w', 'v']
+]
+
+print(shortest_path(edges, 'w', 'z'))
