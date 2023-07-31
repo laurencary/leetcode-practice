21528cbd7f382271f92a99b89602e39ac0ff7c24
laurencary <armstronglg720@gmail.com>
2023-07-31 16:05:18 -0700

    structy graph problems

--- structy/graph.py
+++ structy/graph.py
@@ -122,4 +122,149 @@ edges = [
   ['w', 'v']
 ]
 
-print(shortest_path(edges, 'w', 'z'))
+# print(shortest_path(edges, 'w', 'z'))
+
+def island_count(grid):
+	visited = set()
+	count = 0
+	for r in range(len(grid)):
+		for c in range(len(grid[0])):
+			if grid[r][c] == "L" and (r, c) not in visited:
+				count += 1
+				visited.add((r, c))
+				stack = [(r, c)]
+				while stack:
+					r, c = stack.pop()
+					for neigh_adj in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
+						nr = neigh_adj[0] + r
+						nc = neigh_adj[1] + c
+						is_on_grid = 0 <= nc < len(grid[0]) and 0 <= nr < len(grid)
+						if (nr, nc) not in visited and is_on_grid and grid[nr][nc] == "L":
+							visited.add((nr, nc))
+							stack.append((nr, nc))
+
+	return count
+
+
+grid = [
+  ['W', 'L', 'W', 'W', 'W'],
+  ['W', 'L', 'W', 'W', 'W'],
+  ['W', 'W', 'W', 'L', 'W'],
+  ['W', 'W', 'L', 'L', 'W'],
+  ['L', 'W', 'W', 'L', 'L'],
+  ['L', 'L', 'W', 'W', 'W'],
+]
+
+# print(island_count(grid)) # -> 3
+
+
+
+# def minimum_island(grid):
+#   min_size = len(grid) * len(grid[0])
+#   visited = set()
+
+#   for r in range(len(grid)):
+#     for c in range(len(grid[0])):
+#       if grid[r][c] == "L" and (r, c) not in visited:
+#         size = 0
+#         visited.add((r, c))
+#         stack = [(r, c)]
+#           while stack:
+#             cr, cc = stack.pop()
+#             size += 1
+#             for adjappend((nr, nc))
+#                 visite in [[0,1],[1,0],[0,-1],[-1,0]]:
+#               nr = cr + adj[0]
+#               nc = cc + adj[1]
+#               on_grid = 0 <= nr < len(grid) and 0 <= nc < len(grid[0])
+#               if on_grid and grid[nr][nc] == "L" and (nr, nc) not in visited:
+#                 stack.d.add((nr, nc))
+#       min_size = min(min_size, size)
+
+#   return min_size
+  
+
+grid = [
+  ['W', 'L', 'W', 'W', 'W'],
+  ['W', 'L', 'W', 'W', 'W'],
+  ['W', 'W', 'W', 'L', 'W'],
+  ['W', 'W', 'L', 'L', 'W'],
+  ['L', 'W', 'W', 'L', 'L'],
+  ['L', 'L', 'W', 'W', 'W'],
+]
+
+# print(minimum_island(grid))
+
+def closest_carrot(grid, starting_row, starting_col):
+  visited = set((starting_row, starting_col))
+  queue = deque([(starting_row, starting_col, 0)])
+  neighbors = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+  while queue:
+    print(queue)
+    curr_row, curr_col, level = queue.popleft()
+    print(curr_row, curr_col, grid[curr_row][curr_col])
+    if grid[curr_row][curr_col] == "C":
+      return level
+    for neighbor in neighbors:
+      neigh_row = curr_row + neighbor[0]
+      neigh_col = curr_col + neighbor[1]
+      on_grid = 0 <= neigh_col < len(grid[0]) and 0 <= neigh_row < len(grid)
+      if on_grid and grid[neigh_row][neigh_col] == "O" and (neigh_row, neigh_col) not in visited:
+        visited.add((neigh_row, neigh_col))
+        queue.append((neigh_row, neigh_col, level + 1))
+        
+  return -1
+
+grid = [
+  ['O', 'O', 'O', 'O', 'O'],
+  ['O', 'X', 'O', 'O', 'O'],
+  ['O', 'X', 'X', 'O', 'O'],
+  ['O', 'X', 'C', 'O', 'O'],
+  ['O', 'X', 'X', 'O', 'O'],
+  ['C', 'O', 'O', 'O', 'O'],
+]
+
+# print(closest_carrot(grid, 1, 2)) # -> 4
+
+
+from collections import defaultdict
+
+def semesters_required(num_courses, prereqs):
+  graph = convert_to_adjacency_list(prereqs)
+  distance = {}
+  
+  for node in graph:
+    if len(graph[node]) == 0:
+      distance[node] = 0
+  for node in graph:
+    traverse_distance(graph, node, distance)
+  return max(distance.values()) + 1 
+
+def traverse_distance(graph, node, distance):
+  if node in distance:
+    return distance[node]
+  max_length = 0
+  for neighbor in graph[node]:
+    attempt = traverse_distance(graph, neighbor, distance)
+    max_length = max(max_length, attempt)
+  
+  distance[node] = 1 + max_length
+  return distance[node]
+
+
+def convert_to_adjacency_list(prereqs):
+  graph = defaultdict(list) 
+  for prereq, course in prereqs:
+    graph[prereq].append(course)
+    if course not in graph:
+      graph[course] = []
+  return graph
+
+num_courses = 6
+prereqs = [
+  (1, 2),
+  (2, 4),
+  (3, 5),
+  (0, 5),
+]
+print(semesters_required(num_courses, prereqs)) # -> 3
